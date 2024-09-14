82e6b69b81465ac45a96fb8c32d10e655cff6859
aidyninho <ryzyk.aidyn@gmail.com>
2024-09-15 02:19:46 +0500

    initial commit

---
+++ src/main/java/kz/aidyninho/tipakaspi/TipaKaspiApplication.java
@@ -0,0 +1,13 @@
+package kz.aidyninho.tipakaspi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipaKaspiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TipaKaspiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/aidyninho/tipakaspi/TipaKaspiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aidyninho.tipakaspi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipaKaspiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
