7d2b0ec1599bfe0184a686d92f7f2fc6557f3db7
aidyninho <ryzyk.aidyn@gmail.com>
2024-09-15 06:20:04 +0500

    Add Currency controller

--- src/main/java/kz/aidyninho/tipakaspi/api/CurrencyRestController.java
+++ src/main/java/kz/aidyninho/tipakaspi/api/CurrencyRestController.java
@@ -1,4 +1,25 @@
 package kz.aidyninho.tipakaspi.api;
 
+import kz.aidyninho.tipakaspi.model.Currency;
+import kz.aidyninho.tipakaspi.service.CurrencyService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class CurrencyRestController {
+
+    private final CurrencyService currencyService;
+
+    public CurrencyRestController(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    @PutMapping("/currencies")
+    public List<Currency> updateCurrency() {
+        return currencyService.updateCurrencyRates();
+    }
 }
